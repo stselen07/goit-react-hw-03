e87b93d299ee3368c36973f0bb5005685f7bf81c
sofia tselen <sofiatselenx@gmail.com>
2024-04-18 12:18:25 +0300

    add app css jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState,useEffect } from 'react'
+import ContactForm from './components/contactForm/ContactForm';
+import ContactList from './components/contactList/ContactList';
+import SearchBox from './components/searchBox/SearchBox';
+export default function App() {
 
-function App() {
-  const [count, setCount] = useState(0)
+  const getInitialValues = () => {
+    const storageValues = localStorage.getItem("contacts");
+    return storageValues !== null ?
+      JSON.parse(storageValues)
+      :
+      [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+  };
 
+  const [user, setUser] = useState(getInitialValues);
+
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(user));
+  }, [user]);
+
+  
+  const [search, setSearch] = useState(''); 
+
+  
+  const addUser = (newUser) => {
+    setUser((user) => {
+      return [...user, newUser];
+    })
+  };
+  
+  
+  const deleteUser = (userId) => {
+    setUser((prevUsers) => {
+      return prevUsers.filter((user) => user.id !== userId)
+    });
+  };
+  
+  
+  const visibleContact = user.filter((users) =>
+    users.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+   
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAdd={addUser} />
+      <SearchBox value={search} onSearch={setSearch} />
+      <ContactList data={visibleContact} onDelete={deleteUser} />
+    </div>
+  );
 }
 
-export default App
